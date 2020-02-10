ce3c856f88661e0b868756e312523ea48437beb4
Andrei Ezra C. Gesta | Meister | ECFD <andreiezracago.gesta@rakuten.com>
2020-02-10 09:38:36 +0900

    use bot object to simplify command

--- remnant/main.py
+++ remnant/main.py
@@ -4,28 +4,31 @@
 
 import discord
 
-client = discord.Client()
+from discord.ext import commands
+from discord.ext.commands import Bot
 
-@client.event
+BOT_PREFIX=("$")
+bot = commands.Bot(command_prefix=BOT_PREFIX)
+
+@bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print ("------------------------------------")
+  print ("Bot Name: %s" % bot.user.name)
+  print ("Bot ID: %s" % bot.user.id)
+  print ("Discord Version: %s" % discord.__version__)
+  print ("------------------------------------")
 
-@client.event
-async def on_message(message):
-  """
-  message is from the sender (remnant guild members)
-  client is the bot (remnant-bot#9970)
-  """
-  print('message is from %s' % message.channel)
-  print('getting channel info: %s' % client.get_channel(674876386623881246))
-  print('client user: %s' % client.user)
-  print('message author: %s' % message.author)
-  
-  # avoid processing message from bot
-  if message.author == client.user:
+@bot.command(pass_context=True)
+async def smvp(ctx):
+  message = ctx.message
+  channel = message.channel
+  print("from: %s" % channel)
+  print("message: %s" % message)
+  print("user: %s" % bot.user)
+  print("message author: %s" % message.author)
+  if message.author == bot.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+  await channel.send("Hello!")
 
-client.run('Njc1NTU4ODk4OTYyMzk5Mjcy.Xj48Dw.Z6Fl-H6yoLtMNOXseO7r27jJTBM')
+bot.run("Njc1NTU4ODk4OTYyMzk5Mjcy.Xj6_Bg.8OvcynpH-Hk_a8XoFuRhgPtiuZo")
